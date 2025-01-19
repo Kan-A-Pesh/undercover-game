a496fae8c45c1fd192e93809f42bc93001e90387
AnnamTRAN <annampierretran@gmail.com>
2025-01-19 15:28:46 +0100

    🐛 - FIX socket "connection" file paths

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import { SocketType } from "@/types/socket";
 import { io } from "./server";
 import { glob } from "glob";
+import path from "path";
 
 io.on("connection", async (socket) => {
   // Register events
   const res = await glob(__dirname + "/events/*.ts");
-  const modules = (await Promise.all(res.map((file) => import(file.replace(__dirname, ".").replace(".ts", ""))))) as {
-    default: (s: SocketType) => void;
+  const modules = (await Promise.all(
+    res.map((file) => import(path.join(__dirname,"/../",file.replace(".ts", "")))),
+  )) as {
+    default: (s: SocketType) => void
   }[];
 
   for (const module of modules) module.default(socket);
