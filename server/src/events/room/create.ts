472ce26af283b004d11509ea195993cf1e1220d8
AnnamTRAN <annampierretran@gmail.com>
2025-01-24 02:24:16 +0100

    🚧 - WORKING ON testing callback room create

--- server/src/events/room/create.ts
+++ server/src/events/room/create.ts
@@ -3,19 +3,19 @@ import { SocketType } from "#/socket";
 import jwt from "jsonwebtoken";
 
 export default function create(socket: SocketType) {
-  socket.on("room:create", () => {
+  //! blabla only used for Postman test, remove when done
+  //TODO: remove blabla
+  socket.on("room:create", (blabla, callback) => {
     const player = new Player();
     let token: string;
     try {
       token = jwt.sign({ userId: player.getId() }, process.env.JWT_SECRET!);
       player.createRoom();
 
+      callback(token);
     } catch (error) {
       //TODO: handle error
       console.error(error);
-      return "";
     }
-
-    return token;
   });
 }
